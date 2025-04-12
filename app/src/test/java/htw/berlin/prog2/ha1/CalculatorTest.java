fd1656ba07c1a145233133d1b6222c077721ecb3
KabboudiW <Wael.Kabboudi@Student.HTW-Berlin.de>
2025-04-12 17:21:57 +0200

    test1

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -90,5 +90,20 @@ class CalculatorTest {
 
 
     //TODO hier weitere Tests erstellen
+    @Test
+        @DisplayName("Sollte das Ergebnis nach dem Hinzufügen von zwei positiven mehrstelligen Zahlen angezeigt")
+        void testMultiply() {
+            Calculator calc = new Calculator();
+            calc.pressDigitKey(2);
+            calc.pressDigitKey(0);
+            calc.pressBinaryOperationKey("x");
+            calc.pressDigitKey(2);
+            calc.pressDigitKey(0);
+            calc.pressEqualsKey();
+            String expected = "400";
+            String actual = calc.readScreen();
+            assertEquals(expected, actual);
+        }
+
 }
 
